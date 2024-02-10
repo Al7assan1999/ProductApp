0ca6f5f32c9f9ac29c0878051a5de5d9b893ab34
alhassan.hokan <alhassan.hokan@gmail.com>
2024-02-10 16:01:37 +0300

    Add project files.

---
+++ aspnet-core/src/ProductApp.Application/ProductAppApplicationAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace ProductApp;
+
+public class ProductAppApplicationAutoMapperProfile : Profile
+{
+    public ProductAppApplicationAutoMapperProfile()
+    {
+        /* You can configure your AutoMapper mapping configuration here.
+         * Alternatively, you can split your mapping configurations
+         * into multiple profile classes for a better organization. */
+    }
+}
